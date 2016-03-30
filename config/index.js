1f15dd3d6a64208c429b20c9cdb234f06082b94f
Geir Gåsodden <geir.gasodden@pythonia.no>
2016-03-30 15:11:48 +0200

    Initial commit

---
+++ config/index.js
@@ -0,0 +1,11 @@
+'use strict'
+
+var config = {
+  SERVER_PORT_API: process.env.SERVER_PORT_API || 3000,
+  JWT_SECRET: process.env.JWT_SECRET || 'Louie Louie, oh no, I got to go',
+  COOKIE_SECRET: process.env.COOKIE_SECRET || 'Louie Louie, oh no, I got to go',
+  ELASTIC_URL: process.env.ELASTIC_URL || 'http://elasticsearch',
+  ELASTIC_PORT: process.env.ELASTIC_PORT || '9200'
+}
+
+module.exports = config
