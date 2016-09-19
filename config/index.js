aa3ab29c25c84b524e9b51349fad48db967d0526
Geir Gåsodden <geir.gasodden@pythonia.no>
2016-09-19 08:53:36 +0200

    Doubles secret length

--- config/index.js
+++ config/index.js
@@ -1,10 +1,8 @@
 'use strict'
 
-var config = {
+module.exports = {
   SERVER_PORT: process.env.SERVER_PORT || 3000,
-  JWT_SECRET: process.env.JWT_SECRET || 'Louie Louie, oh no, I got to go',
+  JWT_SECRET: process.env.JWT_SECRET || 'Louie Louie, oh no, I got to go Louie Louie, oh no, I got to go',
   ELASTIC_URL: process.env.ELASTIC_URL || 'http://elasticserver',
   ELASTIC_PORT: process.env.ELASTIC_PORT || '9200'
 }
-
-module.exports = config
