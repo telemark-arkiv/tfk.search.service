57c3deace278922ee354041fdeaf990d6ef12bdc
Geir Gåsodden <geir.gasodden@pythonia.no>
2016-09-22 09:29:56 +0200

    Adds options to token

--- utils/generate-test-token.js
+++ utils/generate-test-token.js
@@ -2,11 +2,15 @@
 
 const generateToken = require('tfk-generate-jwt')
 const config = require('../config')
+const options = {
+  expiresIn: '1h',
+  issuer: 'https://auth.t-fk.no'
+}
 const data = {
   cn: 'Bernhard Riemann',
   userId: 'riemann'
 }
 
-const token = generateToken({payload: data, key: config.JWT_SECRET})
+const token = generateToken({payload: data, key: config.JWT_SECRET, options: options})
 
 console.log(token)
